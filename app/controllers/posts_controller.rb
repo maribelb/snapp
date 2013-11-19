41d9e9d3b6871a7fe8fbfeee391744c876b9600b
student <student@dc-imac-01.dhcp.asu.edu>
2013-11-19 12:11:43 -0700

    posts, finally, showing and indexing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,13 @@ class PostsController < ApplicationController
   end
 
   def index
+	  @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
+    @user = User.find(params[:user_id])
+	  @post = Post.find(params[:id])
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+	has_many :comments
+	has_many :likes
+	has_many :dislikes
 end
